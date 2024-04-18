140d2d28fc51a9e43b2727df159fb4652e84f72b
Bori888 <ilyes.bori8@gmail.com>
2024-04-18 12:14:02 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import { GYUMOLCSOK } from "./adatok.js";
+
+const vasarolt = [];
+
+// 1. feladat
+function letrehozTablazat() {
+
+}
+
+// 2. feladat
+function osszesit() {
+}
+
+// 3. feladat
+function vasarol(zoldseg) {
+
+}
+
+// 4. feladat
+function torolEsemeny() {
+
+}
