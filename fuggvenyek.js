eb85386b7f547776fa6fc40a201a8bf35797b876
Bori888 <ilyes.bori8@gmail.com>
2024-04-18 12:14:51 +0200

    a lista feltoltes kesz

---
+++ fuggvenyek.js
@@ -0,0 +1,17 @@
+export function tablazatLetrehoz(lista) {
+    let txt = "<div class='table-responsive'><table class='table'>";
+    txt += "<thead><tr><th>Név</th><th>tomeg</th><th>ar</th><th></th></tr></thead>";
+    txt += "<tbody>";
+    lista.forEach((elem, index) => {
+        txt += `<tr>
+        <td>${elem.nev}</td>;
+        <td>${elem.tomeg}</td>;
+        <td>${elem.ar}</td>;
+        </tr>`;
+    });
+    
+    txt +="</tbody></table></div>";
+    return txt;
+
+    
+}
